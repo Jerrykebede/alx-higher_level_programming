dcb4adcf039ddc7dd562e8ff0221d12951fc5b26
JerryKebede <eyerusalem_ke@yahoo.com>
2024-02-20 05:57:05 -0800

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""
-module that cointains function to check if an object is an instance of a class
-"""
+"""Defines an inherited class-checking function."""
 
 
-def is_same_class(obj, a_class):
-    """
-    object is the most base type class, a_class
-    cointain instance of the object class
-    """
-# class type(object)
-    if (type(obj) == a_class):
-        # the object is exactly an instance of the a_class
-        return True
-    # otherwise,
-    return False
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to returns:
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+        """
+        if issubclass(type(obj), a_class)and type(obj) != a_class:
+            return True
+        return False
+
