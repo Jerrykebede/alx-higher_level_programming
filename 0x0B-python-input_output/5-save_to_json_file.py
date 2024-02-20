47dc02ed96907871d0a135c599c004f88407f05b
JerryKebede <eyerusalem_ke@yahoo.com>
2024-02-20 05:25:54 -0800

    0-read_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
-
+"""
+Module witch contain function that writes an
+Object to a text file (with JSON)
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
-        json.dump(my_obj, f)
+    with open(filename, mode='w') as f:
+        f.write(json.dumps(my_obj))
