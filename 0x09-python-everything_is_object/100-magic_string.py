a72c9d51aab499cb22c12e366344304fa4b5b8ac
JerryKebede <eyerusalem_ke@yahoo.com>
2024-03-01 06:11:43 -0800

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(static={"count": 0}):
-    static["count"] += 1
-    return ', '.join(["Holberton" for i in range(static["count"])])
+def magic_string(count=[-1]):
+    count[0] += 1
+    return "Holberton" + ", Holberton" * count[0]
