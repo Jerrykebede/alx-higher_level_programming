8240c4f060949787a0de0d33266edce9d8060e36
JerryKebede <eyerusalem_ke@yahoo.com>
2024-03-12 22:53:43 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# 11-student.py
 """Defines a class Student."""
 
 
@@ -7,6 +8,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
+
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -16,14 +18,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict___
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
