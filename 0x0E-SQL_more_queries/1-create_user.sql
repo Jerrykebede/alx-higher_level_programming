6c568d30c0020e9b42e5e7287637a58cdef959c9
JerryKebede <eyerusalem_ke@yahoo.com>
2024-05-22 12:47:24 -0700

    text

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,2 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Creates a database in the server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
