c53d20993e54dc3d3c1778976cae7fd32db69eeb
JerryKebede <eyerusalem_ke@yahoo.com>
2024-06-23 07:00:26 -0700

    done

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Lists states"""
-
+"""
+    Improve the file model_state.py, and save as relationship_state.py
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
-
+from sqlalchemy.orm import relationship
 Base = declarative_base()
+""" Instance """
 
 
 class State(Base):
-    """Class representing the states table"""
+    """ Class """
     __tablename__ = 'states'
-
-    id = Column(Integer, nullable=False, primary_key=True,
-                autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship(
-        "City",
-        cascade="all, delete-orphan",
-        backref=backref("state", cascade="all"),
-        single_parent=True)
+    cities = relationship("City", backref="state")
